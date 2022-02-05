5063eb56b83f51ead667c9609500a7b9096bd6c1
felix-lipski <felix.lipski7@gmail.com>
2022-02-05 15:13:25 +0000

    read roots

--- src/main/scala/example/Hello.scala
+++ src/main/scala/example/Hello.scala
@@ -1,5 +1,6 @@
 package example
 
+import scala.io.Source
 import zio._
 import zio.process._
 import java.io._
@@ -7,13 +8,17 @@ import java.io._
 object Main extends scala.App {
   val program =
     for {
-       _ <- console.putStrLn("home dir?")
-       homeDir <- ZIO.succeed("/home/felix/")
-       _ <- console.putStrLn(s"Your home is at: $homeDir")
-       _ <- console.putStrLn("─" * 11)
+      roots <- ZIO.succeed(
+        Source.fromFile("/home/felix/.config/flaker/roots").getLines()
+      )
+      _ <- console.putStrLn("home dir?")
+      homeDir <- ZIO.succeed("/home/felix/")
+      _ <- console.putStrLn(s"Your home is at: $homeDir")
+      _ <- console.putStrLn("─" * 11)
 
-       flakeDirs <- Command("find", "/home/felix/", "-name", "flake.lock", "-type", "f").string
-       _ <- console.putStrLn(flakeDirs)
+      flakeDirs <- Command("find", "/home/felix/", "-name", "flake.lock", "-type", "f").string
+      _ <- console.putStrLn(flakeDirs)
+      _ <- console.putStrLn(roots.foldLeft("")((a,b) => a ++ "\n" ++ b))
     } yield ()
 
   Runtime.default.unsafeRunSync(program)
