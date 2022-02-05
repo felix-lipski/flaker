8c9163061e470f846ad0873272569af81ad9dba3
felix-lipski <felix.lipski7@gmail.com>
2022-02-05 18:43:48 +0000

    find all flake locks in roots

--- src/main/scala/example/Hello.scala
+++ src/main/scala/example/Hello.scala
@@ -3,22 +3,17 @@ package example
 import scala.io.Source
 import zio._
 import zio.process._
-import java.io._
 
 object Main extends scala.App {
   val program =
     for {
-      roots <- ZIO.succeed(
-        Source.fromFile("/home/felix/.config/flaker/roots").getLines()
-      )
-      _ <- console.putStrLn("home dir?")
-      homeDir <- ZIO.succeed("/home/felix/")
-      _ <- console.putStrLn(s"Your home is at: $homeDir")
-      _ <- console.putStrLn("─" * 11)
+      roots <- ZIO.effectTotal(Source.fromFile("/home/felix/.config/flaker/roots").getLines().toIterable)
 
-      flakeDirs <- Command("find", "/home/felix/", "-name", "flake.lock", "-type", "f").string
-      _ <- console.putStrLn(flakeDirs)
-      _ <- console.putStrLn(roots.foldLeft("")((a,b) => a ++ "\n" ++ b))
+      lockPaths <- ZIO
+        .foreach(roots)(Command("find", _, "-name", "flake.lock", "-type", "f").string)
+        .map(_.flatMap(_.split("\n")))
+
+      _ <- console.putStrLn(lockPaths.toString())
     } yield ()
 
   Runtime.default.unsafeRunSync(program)
